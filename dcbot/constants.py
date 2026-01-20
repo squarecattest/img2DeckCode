fb1cb04d1c53542742b13c6271af38a074baabe5
squarecat71522 <huliou880@gmail.com>
2026-01-20 22:58:28 +0900

    create file

---
+++ dcbot/constants.py
@@ -0,0 +1,13 @@
+from enum import StrEnum
+
+__all__ = ("emoji",)
+
+class emoji(StrEnum):
+    effect = ""
+    spell = ""
+    trap = ""
+    fusion = ""
+    synchro = ""
+    xyz = ""
+    link = ""
+
